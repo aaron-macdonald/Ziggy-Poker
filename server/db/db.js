493cf90019037e95241ef897c46ba4f6db7c27c6
Your Name <you@example.com>
2017-10-07 13:00:43 +1300

    Initial

--- server/db/db.js
+++ server/db/db.js
@@ -2,5 +2,12 @@ const path = require('path')
 const config = require(path.join(__dirname, '/../../knexfile')).development
 const knex = require('knex')(config)
 
+function getPosts(){
+  return knex('Posts')
+}
+
+
+
 module.exports = {
+  getPosts,
 }
